d73d788dff901e00ae5d44b10b3767ffe2850965
Leonardo Taian <leonardotaianav@outlook.com>
2024-02-03 16:25:28 -0300

    começando rotas/sql querys

--- app.py
+++ app.py
@@ -13,14 +13,26 @@ from flask import Flask, render_template, request, url_for
 # tipo_produto
 # racao, acessorio, medicamento, higienie, brinquedo
 
-
-
-
 app = Flask(__name__)
 @app.route("/")
 def inicial():
     return render_template ('index.html')
 
+@app.route("/servicos")
+def servicos():
+    return render_template ('servicos.html')
+
+@app.route("/login")
+def login():
+    return render_template ('login.html')
+
+@app.route("/cadastro")
+def cadastro():
+    return render_template ('cadastro.html')
+
+@app.route("/promocao")
+def promocao():
+    return render_template ('promocao.html')
 
 app.run(debug=True)
 
--- rep.py
+++ rep.py
@@ -1,6 +1,20 @@
 import sqlite3
 
 
+def cadastro(nome,email,cpf,celular,endereco,senha,pet,nome_pet):
+    try:
+        conn = sqlite3.connect('petshop.db')
+        cursor = conn.cursor()
+        sql_select = f'INSERT INTO humano (nome_humano,cpf_humano,email_humano,contato_humano,endereco_humano,senha) VALUES ("nome","cpf","email","celular","endereco","senha")'
+        cursor.execute(sql_select)
+        sql_select_pet = f'INSERT INTO clientes (nome_cliente, id_animal) VALUES ("nome_pet","pet")'
+        cursor.execute(sql_select_pet)
+        conn.commit()
+        conn.close()
+        msg = ("Dados gravados com sucesso")
+    except:
+        msg = ("Erro ao inserir os dados")
+
 def produto(tipo_produto,id_animal:int):
     try:
         conn = sqlite3.connect('petshop.db')
